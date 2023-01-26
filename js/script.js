9fde049f72012de29fa8288b9a9391ef2bdc2c63
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-01-26 02:16:16 -0300

    Tela 2 e 3 Estrutura básica do JS pra enviar novo quiz

--- js/script.js
+++ js/script.js
@@ -1,3 +1,96 @@
+/* BOTOES TELA DE NOVO QUIZZ */
+
+function toggleVisualisarPerguntas(ionicon) {
+    //Buscar o container-pai
+    console.log(ionicon.parentNode);
+    //Mudar a classe do container adicionando a classe .esconde
+    ionicon.parentNode.classList.toggle("esconde");
+}
+
+function prosseguirPraCriarPerguntas() {
+    //Buscar o container-tela2
+    const containerTela2 = document.querySelector(".container-tela2");
+
+    //remover deste a classe visivel
+    containerTela2.classList.remove("visivel");
+
+    //Buscar o container-tela3
+    const containerTela3 = document.querySelector(".container-tela3");
+
+    //adicionar ao container-tela3 a classe visível
+    containerTela3.classList.add("visivel");
+
+    //adicionar as perguntas com base na quantidade de perguntas na pagina anterior
+    let perguntasHTML = `
+    <p class="titulo-tela3">Crie suas perguntas</p>
+    <div class="container-tela3-a pergunta">
+        <ion-icon name="create-outline" onclick="toggleVisualisarPerguntas(this)"></ion-icon>
+        <p class="titulo-pergunta contexto-pergunta">Pergunta <span class="numero-pergunta">1</span></p>
+        <input type="text" name="title" id="" placeholder="Texto da pergunta">
+        <input type="color" name="color" id="" placeholder="Cor de fundo da pergunta">
+        <p class="titulo-pergunta">Resposta correta</p>
+        <input type="text" name="text" id="" placeholder="Resposta correta">
+        <input type="url" name="image" id="" placeholder="URL da imagem">
+        <p class="titulo-pergunta">Respostas incorretas</p>
+        <input type="text" name="text" id="" placeholder="Resposta incorreta 1">
+        <input type="url" name="image" id="" placeholder="URL da imagem 1">
+        <input type="text" name="text" id="" placeholder="Resposta incorreta 2">
+        <input type="url" name="image" id="" placeholder="URL da imagem 2">
+        <input type="text" name="text" id="" placeholder="Resposta incorreta 3">
+        <input type="url" name="image" id="" placeholder="URL da imagem 3">
+    </div>
+    `;
+    
+    for (let i = 2; i <= document.querySelector("#numero-de-perguntas").value; i++) {
+        perguntasHTML += `
+    <div class="container-tela3-a pergunta esconde">
+        <ion-icon name="create-outline" onclick="toggleVisualisarPerguntas(this)"></ion-icon>
+        <p class="titulo-pergunta contexto-pergunta">Pergunta <span class="numero-pergunta">${i}</span></p>
+        <input type="text" name="title" id="" placeholder="Texto da pergunta">
+        <input type="color" name="color" id="" placeholder="Cor de fundo da pergunta">
+        <p class="titulo-pergunta">Resposta correta</p>
+        <input type="text" name="text" id="" placeholder="Resposta correta">
+        <input type="url" name="image" id="" placeholder="URL da imagem">
+        <p class="titulo-pergunta">Respostas incorretas</p>
+        <input type="text" name="text" id="" placeholder="Resposta incorreta 1">
+        <input type="url" name="image" id="" placeholder="URL da imagem 1">
+        <input type="text" name="text" id="" placeholder="Resposta incorreta 2">
+        <input type="url" name="image" id="" placeholder="URL da imagem 2">
+        <input type="text" name="text" id="" placeholder="Resposta incorreta 3">
+        <input type="url" name="image" id="" placeholder="URL da imagem 3">
+    </div>        
+    `;
+    }
+
+    perguntasHTML += `
+    <button type="submit" id="button-tela3" onclick="prosseguirPraCriarNiveis()">
+        Prosseguir pra criar níveis
+    </button>
+    `;
+
+    containerTela3.innerHTML = perguntasHTML;
+}
+
+function prosseguirPraCriarNiveis() {
+    //Buscar o container-tela3
+    const containerTela3 = document.querySelector(".container-tela3");
+    //remover deste a classe visivel
+    containerTela3.classList.remove("visivel");
+    //Buscar o container-tela4
+    const containerTela4 = document.querySelector(".container-tela4");
+    //adicionar ao container-tela4 a classe visível
+    containerTela4.classList.add("visivel");
+}
+
+
+
+
+
+
+
+
+
+
 const formNovoQuiz = document.querySelector("#form-novo-quiz");
 
 formNovoQuiz.addEventListener("submit", function(evento) {
